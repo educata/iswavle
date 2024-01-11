370df5ec4cd9faedfc873a0e5f8c3d87df44c65e
Kosta <68782786+KostaD02@users.noreply.github.com>
2024-01-11 15:52:29 +0400

    docs: guides/javascript/array

--- src/content/examples/guides/javascript-array/main.js
+++ src/content/examples/guides/javascript-array/main.js
@@ -21,6 +21,9 @@ const matrix = [
 
 console.log(matrix);
 
+const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(...numbersArray); // 1 2 3 4 5 6 7 8 9 10
+
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(array.length);
 
