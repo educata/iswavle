e5155a835e6ba9b14d58e043639a42957c5662ec
Konstantine Datunishvili <68782786+KostaD02@users.noreply.github.com>
2024-03-16 01:06:04 +0400

    feat: handle anchor click (#36)
    Co-authored-by: Pridon Tetradze <pridontetradze@gmail.com>

--- src/app/features/docs/ui/doc-viewer/doc-viewer.component.ts
+++ src/app/features/docs/ui/doc-viewer/doc-viewer.component.ts
@@ -4,9 +4,13 @@ import {
   ElementRef,
   Input,
   OnChanges,
+  Renderer2,
   SimpleChanges,
   ViewChild,
+  inject,
 } from '@angular/core';
+import { Router } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
 import { DocContent } from '@app-shared/interfaces';
 
 @Component({
@@ -21,6 +25,10 @@ export class DocViewerComponent implements OnChanges {
   @Input() docContent!: DocContent;
   @ViewChild('container', { static: true }) container!: ElementRef<HTMLElement>;
 
+  private readonly renderer = inject(Renderer2);
+  private readonly router = inject(Router);
+  private readonly viewport = inject(ViewportScroller);
+
   ngOnChanges(changes: SimpleChanges) {
     if ('docContent' in changes) {
       this.renderPage(this.docContent.content);
@@ -30,5 +38,22 @@ export class DocViewerComponent implements OnChanges {
   renderPage(content: string) {
     const contentContainer = this.container.nativeElement;
     contentContainer.innerHTML = content;
+
+    this.renderer.listen(contentContainer, 'click', (event) => {
+      if (event.target instanceof HTMLAnchorElement) {
+        this.handleAnchorClick(event);
+      }
+    });
+  }
+
+  handleAnchorClick(event: Event) {
+    const a = event.target as HTMLAnchorElement;
+    if (a?.target || /^https:\/\/.*/.test(a.href) || /^#.*/.test(a.href)) {
+      return;
+    }
+    event.preventDefault();
+    const currentDomain = a.href.split('/').slice(0, 3).join('/');
+    this.router.navigateByUrl(a.href.replace(currentDomain, ''));
+    this.viewport.scrollToPosition([0, 0]);
   }
 }
