d22fcbc8bce2c9c50b0fb092472cf11dd21b27ce
Kosta <68782786+KostaD02@users.noreply.github.com>
2024-03-16 17:55:16 +0400

    feat: search breadcrumb

---
+++ src/app/shared/ui/search-result/search-breadcrumb.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'searchBreadcrumb',
+  standalone: true,
+})
+export class SearchBreadcrumbPipe implements PipeTransform {
+  transform(value: string) {
+    return value
+      .split('/')
+      .map((element) => {
+        return element
+          .split('')
+          .map((symbol, index) => (index === 0 ? symbol.toUpperCase() : symbol))
+          .join('');
+      })
+      .slice(0, -1)
+      .join(' > ')
+      .concat(' > ');
+  }
+}
