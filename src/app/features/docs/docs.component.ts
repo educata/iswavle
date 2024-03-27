570e93a7071b2c01d5a28aa051b3a9a7cb1ab651
Konstantine Datunishvili <68782786+KostaD02@users.noreply.github.com>
2024-03-27 20:47:12 +0400

    refactor: toc (#59)

--- src/app/features/docs/docs.component.ts
+++ src/app/features/docs/docs.component.ts
@@ -125,6 +125,10 @@ export default class DocsComponent {
     () => !(this.windowWidth()! >= this.layoutServie.sizes.hideToc),
   );
 
+  readonly isEnoughToc = computed(
+    () => (this.article()?.attributes.toc.length || 0) > 1,
+  );
+
   constructor() {
     effect(() => {
       const content = this.article();
