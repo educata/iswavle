5210064464d5c0a39a2b591c83673086915ee0e2
Kosta <68782786+KostaD02@users.noreply.github.com>
2024-04-06 13:41:31 +0400

    feat: comments

---
+++ src/app/shared/ui/comments/comments.component.ts
@@ -0,0 +1,42 @@
+import { isPlatformBrowser } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  PLATFORM_ID,
+  Renderer2,
+  ViewContainerRef,
+  inject,
+} from '@angular/core';
+
+@Component({
+  selector: 'sw-comments',
+  standalone: true,
+  imports: [],
+  templateUrl: './comments.component.html',
+  styleUrl: './comments.component.less',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CommentsComponent implements OnInit {
+  private readonly renderer = inject(Renderer2);
+  private readonly vcr = inject(ViewContainerRef);
+  private readonly isBrowser = isPlatformBrowser(inject(PLATFORM_ID));
+
+  ngOnInit(): void {
+    if (this.isBrowser) {
+      const script = this.renderer.createElement('script');
+      this.renderer.setAttribute(
+        script,
+        'src',
+        'https://utteranc.es/client.js',
+      );
+      this.renderer.setAttribute(script, 'repo', 'educata/iswavle');
+      this.renderer.setAttribute(script, 'issue-term', 'pathname');
+      this.renderer.setAttribute(script, 'label', '💬');
+      this.renderer.setAttribute(script, 'theme', 'github-light');
+      this.renderer.setAttribute(script, 'crossorigin', 'anonymous');
+      this.renderer.setAttribute(script, 'async', '');
+      (this.vcr.element.nativeElement as HTMLElement).appendChild(script);
+    }
+  }
+}
--- src/app/shared/ui/index.ts
+++ src/app/shared/ui/index.ts
@@ -3,3 +3,4 @@ export * from './auto-breadcrumbs/auto-breadcrumbs.component';
 export * from './loader/loader.component';
 export * from './search-result/search-result.component';
 export * from './search/search.component';
+export * from './comments/comments.component';
