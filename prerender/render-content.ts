c15e773d75c17c6097d9adefbf0ac2b913e93649
Konstantine Datunishvili <68782786+KostaD02@users.noreply.github.com>
2024-05-11 14:29:29 +0400

    feat: custom blocks & mermaid (#88)
    Co-authored-by: Pridon Tetradze <pridontetradze@gmail.com>

--- prerender/render-content.ts
+++ prerender/render-content.ts
@@ -48,7 +48,40 @@ render.table = (header: string, body: string) => {
   return `<div class="table-wrapper"><table><thead>${header}</thead><tbody>${body}</tbody></table></div>`;
 };
 
+render.paragraph = (text) => {
+  if (text.startsWith(':::') && text.endsWith(':::')) {
+    return registerCustomBlocks(text);
+  }
+
+  return `<p>${text}</p>`;
+};
+
 marked.setOptions({ renderer: render });
+
+function registerCustomBlocks(text: string) {
+  const block = text.split('\n')[0].replace(':::', '');
+
+  switch (block.toLocaleLowerCase()) {
+    case 'mermaid': {
+      return `<div class="mermaid">${text.slice(11, -4)}</div>`;
+    }
+    case 'success': {
+      return `<div class="note ant-alert ant-alert-success">${text.slice(11, -4)}</div>`;
+    }
+    case 'warning': {
+      return `<div class="note ant-alert ant-alert-warning">${text.slice(11, -4)}</div>`;
+    }
+    case 'error': {
+      return `<div class="note ant-alert ant-alert-error">${text.slice(9, -4)}</div>`;
+    }
+    case 'info': {
+      return `<div class="note ant-alert ant-alert-info">${text.slice(8, -4)}</div>`;
+    }
+    default: {
+      return `<div class="note ant-alert ant-alert-info">${text.slice(4, -4)}</div>`;
+    }
+  }
+}
 
 function extractHeaders(htmlString: string): ArticleToc[] {
   const result: ArticleToc[] = [];
