17fcb679e1a500a3319b68d1bf3e4d63f1921927
Kosta <68782786+KostaD02@users.noreply.github.com>
2024-08-10 19:53:21 +0400

    fix: duplicated fn

--- prerender/render-content.ts
+++ prerender/render-content.ts
@@ -91,31 +91,6 @@ function registerCustomBlocks(text: string) {
   return text;
 }
 
-function registerCustomBlocks(text: string) {
-  const block = text.split('\n')[0].replace(':::', '');
-
-  switch (block.toLocaleLowerCase()) {
-    case 'mermaid': {
-      return `<div class="mermaid">${text.slice(11, -4)}</div>`;
-    }
-    case 'success': {
-      return `<div class="note ant-alert ant-alert-success">${text.slice(11, -4)}</div>`;
-    }
-    case 'warning': {
-      return `<div class="note ant-alert ant-alert-warning">${text.slice(11, -4)}</div>`;
-    }
-    case 'error': {
-      return `<div class="note ant-alert ant-alert-error">${text.slice(9, -4)}</div>`;
-    }
-    case 'info': {
-      return `<div class="note ant-alert ant-alert-info">${text.slice(8, -4)}</div>`;
-    }
-    default: {
-      return `<div class="note ant-alert ant-alert-info">${text.slice(4, -4)}</div>`;
-    }
-  }
-}
-
 function extractHeaders(htmlString: string): ArticleToc[] {
   const result: ArticleToc[] = [];
 
