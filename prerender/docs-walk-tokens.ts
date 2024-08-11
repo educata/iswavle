31028325fe7131568da685d6e1f0b4412d70eadb
Kosta <68782786+KostaD02@users.noreply.github.com>
2024-08-11 13:23:09 +0400

    fix: mermaid

--- prerender/docs-walk-tokens.ts
+++ prerender/docs-walk-tokens.ts
@@ -1,8 +1,10 @@
+import { cpSync, existsSync, readFileSync, rmSync, writeFileSync } from 'fs';
 import { Tokens } from 'marked';
 import { run } from '@mermaid-js/mermaid-cli';
-import { existsSync, readFileSync, rmSync, writeFileSync } from 'fs';
+import path from 'path';
 
 const srcAssetsDir = './src/assets';
+const notFoundSvgPath = path.join(__dirname, 'assets', 'not-found.svg');
 
 export async function docsWalkTokens(walkToken: Tokens.Generic): Promise<void> {
   if (
@@ -22,13 +24,31 @@ async function handleMermaid(token: Tokens.Code) {
   try {
     writeFileSync(inputPath, token.text, 'utf-8');
 
-    await run(inputPath, outputPath);
+    await run(inputPath, outputPath, {
+      parseMMDOptions: {
+        mermaidConfig: {
+          themeCSS: `
+            background-color: var(--mermaid-bg-color, transparent) !important;
+            margin: auto;
+            display: block;
+          `,
+        },
+      },
+    });
+
+    if (!existsSync(outputPath)) {
+      cpSync(notFoundSvgPath, outputPath);
+      console.warn(
+        `⚠ Mermaid diagram could not be rendered, using default not-found.svg`,
+      );
+    }
 
     token.text = `<div data-search-ignore class="mermaid">${readFileSync(outputPath, 'utf-8')}</div>`;
 
     existsSync(inputPath) && rmSync(inputPath);
     existsSync(outputPath) && rmSync(outputPath);
   } catch (error) {
-    console.error('Error rendering mermaid diagram: ', error);
+    const { message } = error as Error;
+    console.error(`❌ Error while rendering mermaid: ${message}}`);
   }
 }
