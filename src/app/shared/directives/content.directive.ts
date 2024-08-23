747261e99132d4bb970f0a11fdc7ee67cd4a9174
Kosta <68782786+KostaD02@users.noreply.github.com>
2024-08-23 10:46:22 +0400

    fix: crossorigin load

--- src/app/shared/directives/content.directive.ts
+++ src/app/shared/directives/content.directive.ts
@@ -61,6 +61,7 @@ export class ContentDirective implements OnChanges {
     });
 
     this.renderIframes(body);
+    this.appendCrossOrigin(body);
 
     if (searchKey) {
       const segments = (body.innerHTML as string).split(/(<[^>]+>)/);
@@ -147,6 +148,7 @@ export class ContentDirective implements OnChanges {
 
     if (iframes.forEach) {
       iframes.forEach((iframe: HTMLIFrameElement) => {
+        iframe.setAttribute('crossorigin', 'anonymous');
         const isExternalSource =
           iframe.getAttribute('data-is-external-source') || false;
         const url = iframe.getAttribute('data-url') || '';
@@ -197,5 +199,18 @@ export class ContentDirective implements OnChanges {
         iframe.replaceWith(newFrame);
       });
     }
+  }
+
+  private appendCrossOrigin(body: HTMLDivElement) {
+    const images = body.querySelectorAll('img');
+    const regexForCrossOrigin = new RegExp('https://.*', 'g');
+
+    images.forEach((image) => {
+      const isCrossOrigin = regexForCrossOrigin.test(image.src);
+
+      if (isCrossOrigin) {
+        image.setAttribute('crossorigin', 'anonymous');
+      }
+    });
   }
 }
