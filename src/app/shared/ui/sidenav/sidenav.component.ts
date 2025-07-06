e1ab212ade7bf61658238c72a41f3d175aa7287a
Kosta <68782786+KostaD02@users.noreply.github.com>
2025-07-06 19:12:15 +0400

    fix: navigation nested padding

--- src/app/shared/ui/sidenav/sidenav.component.ts
+++ src/app/shared/ui/sidenav/sidenav.component.ts
@@ -6,6 +6,7 @@ import {
   Input,
   Output,
   TemplateRef,
+  ViewEncapsulation,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -34,6 +35,7 @@ import { NavItemDirective } from './nav-item.directive';
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
 })
 export class SidenavComponent {
   @Input() navItems: NavigationTreeNode[] = [];
@@ -42,6 +44,8 @@ export class SidenavComponent {
   menuTpl?: TemplateRef<unknown>;
 
   @Output() onClick = new EventEmitter<NzMenuItemComponent>();
+
+  readonly paddingSize = 24;
 
   click(event: NzMenuItemComponent) {
     this.onClick.emit(event);
