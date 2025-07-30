22a20cafb201b5d7a4846034d9abcf13cb699030
Kosta <68782786+KostaD02@users.noreply.github.com>
2025-07-30 19:51:17 +0400

    chore: modify server.ts based on netlify/angular-runtime

--- src/server.ts
+++ src/server.ts
@@ -1,48 +1,14 @@
-import {
-  AngularNodeAppEngine,
-  isMainModule,
-  writeResponseToNodeResponse,
-} from '@angular/ssr/node';
-import express from 'express';
-import { join } from 'node:path';
+import { AngularAppEngine, createRequestHandler } from '@angular/ssr';
 import { getContext } from '@netlify/angular-runtime/context';
-import { createRequestHandler } from '@angular/ssr';
 
-const browserDistFolder = join(import.meta.dirname, '../browser');
-
-const app = express();
-const angularApp = new AngularNodeAppEngine();
-
-app.use(
-  express.static(browserDistFolder, {
-    maxAge: '1y',
-    index: false,
-    redirect: false,
-  }),
-);
+const angularApp = new AngularAppEngine();
 
 export async function netlifyAppEngineHandler(
   request: Request,
 ): Promise<Response> {
   const context = getContext();
-  const result = await angularApp.handle(request as any, context);
+  const result = await angularApp.handle(request, context);
   return result || new Response('Not found', { status: 404 });
-}
-
-app.use((req, res, next) => {
-  angularApp
-    .handle(req)
-    .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
-    )
-    .catch(next);
-});
-
-if (isMainModule(import.meta.url)) {
-  const port = process.env['PORT'] || 4000;
-  app.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
-  });
 }
 
 export const reqHandler = createRequestHandler(netlifyAppEngineHandler);
