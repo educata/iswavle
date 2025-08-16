09a8e009b77f35997c1d1915991e66f3a9b2817f
Konstantine Datunishvili <68782786+KostaD02@users.noreply.github.com>
2025-08-16 18:20:27 +0400

    fix: improve HTML tag removal in markdown processing (#159)

--- prerender/render-content.ts
+++ prerender/render-content.ts
@@ -199,8 +199,9 @@ async function processMarkdownFiles(directory: string) {
 function removePreAndHtmlTags(content: string) {
   return content
     .replace(/<pre>.*?<\/pre>/gs, '')
+    .replace(/<(\w+)([^>]*data-search-ignore[^>]*)>([\s\S]*?)<\/\1>/gi, '')
     .replace(/<\/?[^>]*>/g, '')
-    .replace(/<[^>]*data-search-ignore[^>]*>[^<]*<\/[^>]*>/gim, '')
+    .replace(/<[^>]*data-search-ignore[^>]*\s*\/?>/gi, '')
     .replaceAll('\n', ' ');
 }
 
