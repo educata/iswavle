a42e71f68b347afd49498d8c973be1e8ec027e0a
Kosta <68782786+KostaD02@users.noreply.github.com>
2025-08-30 12:35:04 +0400

    fix: ensure browser checks before manipulating DOM in app and theme services

--- src/app/shared/services/layout.service.ts
+++ src/app/shared/services/layout.service.ts
@@ -1,14 +1,24 @@
-import { Injectable, computed, inject, signal, DOCUMENT } from '@angular/core';
+import {
+  Injectable,
+  computed,
+  inject,
+  signal,
+  DOCUMENT,
+  PLATFORM_ID,
+} from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { isPlatformBrowser } from '@angular/common';
 import { LAYOUT_SIZES } from '@app-shared/providers';
 import { fromEvent, map, startWith } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class LayoutService {
   private readonly document = inject(DOCUMENT);
+  private readonly platform = inject(PLATFORM_ID);
+  private readonly isBrowser = isPlatformBrowser(this.platform);
   readonly sizes = inject(LAYOUT_SIZES);
 
-  readonly #windowWidth = signal<number>(this.document.body.clientWidth);
+  readonly #windowWidth = signal<number>(this.sizes.header + 1);
 
   get windowWidth() {
     return this.#windowWidth.asReadonly();
@@ -19,13 +29,18 @@ export class LayoutService {
   );
 
   constructor() {
-    fromEvent(this.document.defaultView as Window, 'resize')
-      .pipe(
-        takeUntilDestroyed(),
-        map((event) => (event.target as Window).innerWidth),
-        startWith(this.document.body.clientWidth),
-      )
-      .subscribe((width) => this.#windowWidth.set(width));
+    if (this.isBrowser) {
+      this.#windowWidth.set(
+        this.document.defaultView?.innerWidth || this.sizes.header + 1,
+      );
+
+      fromEvent(this.document.defaultView as Window, 'resize')
+        .pipe(
+          takeUntilDestroyed(),
+          map((event) => (event.target as Window).innerWidth),
+        )
+        .subscribe((width) => this.#windowWidth.set(width));
+    }
   }
 
   isWiderThan(toCompare: number) {
