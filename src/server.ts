fb34a9cf9abf5643c9d121325ec74b5ca0e88e4c
Kosta <68782786+KostaD02@users.noreply.github.com>
2025-08-30 14:02:31 +0400

    chore: server.ts update (WIP)

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,23 @@
 import { CommonEngine } from '@angular/ssr/node';
-import { render } from '@netlify/angular-runtime/common-engine.mjs';
+import bootstrap from './main.server';
+import { APP_BASE_HREF } from '@angular/common';
+import { fileURLToPath } from 'node:url';
+import { dirname, join, resolve } from 'node:path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const browserDistFolder = resolve(__dirname, '../browser');
+const indexHtml = join(__dirname, 'index.server.html');
 
 const commonEngine = new CommonEngine();
 
-export async function netlifyCommonEngineHandler(
-  request: Request,
-  context: any,
-): Promise<Response> {
-  return await render(commonEngine);
+export async function netlifyCommonEngineHandler(request: Request) {
+  const url = request.url;
+
+  return await commonEngine.render({
+    bootstrap,
+    documentFilePath: indexHtml,
+    url,
+    publicPath: browserDistFolder,
+    providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
+  });
 }
