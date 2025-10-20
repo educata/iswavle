9a397a1cc7f9e410fc5cfc1900c4e31df22d6cde
Pridon Tetradze <pridontetradze@gmail.com>
2025-10-20 22:42:40 +0400

    chore: shell lib boilerplate

---
+++ libs/shell/src/lib/shell/shell.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sw-shell',
+  imports: [],
+  templateUrl: './shell.html',
+  styleUrl: './shell.less',
+})
+export class Shell {}
