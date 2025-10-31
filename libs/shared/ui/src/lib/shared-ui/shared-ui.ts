1ba8e304e2a143452a9bb97d57e0768564667ff7
Pridon Tetradze <pridontetradze@gmail.com>
2025-11-01 01:35:51 +0400

    chore: remove shared UI component and associated files

--- libs/shared/ui/src/lib/shared-ui/shared-ui.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'sw-shared-ui',
-  imports: [],
-  templateUrl: './shared-ui.html',
-  styleUrl: './shared-ui.less',
-})
-export class SharedUi {}
